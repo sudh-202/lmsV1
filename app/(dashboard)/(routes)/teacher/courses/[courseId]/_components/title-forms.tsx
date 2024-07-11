4395fad9c17abf6ef9db7ea145063922943d1525
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-07-11 18:19:09 +0530

    first commit

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-forms.tsx
@@ -0,0 +1,90 @@
+'use client';
+import React, { useState } from 'react';
+import * as z from 'zod';
+import axios from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Pencil } from 'lucide-react';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+type TitleFormsProps = {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+};
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: 'Title is required',
+  }),
+});
+
+export default function TitleForms({ courseId, initialData }: TitleFormsProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success('Course updated');
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error('Something went wrong');
+    }
+  };
+  return (
+    <div className='mt-6 boder bg-slate-100 rounded-md p-4'>
+      <div className='font-medium flex items-center justify-between gapy1'>
+        Course title
+        <Button onClick={toggleEdit} variant={'ghost'}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className='h-4 w-4 mr-2' />
+              Edit title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing ? (
+        <p className='text-sm mt-2'>{initialData.title}</p>
+      ) : (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name='title'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input disabled={isSubmitting} placeholder='e.g "Advance web development"' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='flex items-center mt-2 gap-x-2'>
+              <Button variant="black" disabled={!isValid || isSubmitting}>Save</Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+} 
---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
+const CoursesPage = () => {
+  return (
+    <div className='p-6'>
+      <Link href='/teacher/create'>
+        <Button variant="outline">New Courses</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default CoursesPage;
